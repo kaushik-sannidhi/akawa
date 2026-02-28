f5953d1e6568fecdbd0bad61b13e8d35ab83f885
kaush <kaushik.sannidhi@gmail.com>
2026-02-27 21:56:26 -0600

    updated

--- frontend/src/lib/firebase.ts
+++ frontend/src/lib/firebase.ts
@@ -2,14 +2,14 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA10gg4dmWR19i-5YmbTnkDFxvNt5ghdfY",
-    authDomain: "uiuc-24fae.firebaseapp.com",
-    databaseURL: "https://uiuc-24fae-default-rtdb.firebaseio.com",
-    projectId: "uiuc-24fae",
-    storageBucket: "uiuc-24fae.firebasestorage.app",
-    messagingSenderId: "410939547913",
-    appId: "1:410939547913:web:4e592b9288c76e6ca0f3d8",
-    measurementId: "G-713MQXD177"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyA10gg4dmWR19i-5YmbTnkDFxvNt5ghdfY",
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "uiuc-24fae.firebaseapp.com",
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL || "https://uiuc-24fae-default-rtdb.firebaseio.com",
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "uiuc-24fae",
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "uiuc-24fae.firebasestorage.app",
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "410939547913",
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:410939547913:web:4e592b9288c76e6ca0f3d8",
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-713MQXD177"
 };
 
 
